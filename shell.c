c17c37c6cfabc9d7868e0babb5423f894f39bd2e
gabrielabdul <gabrielabdul1@gmail.com>
2020-04-05 00:12:19 +0000

    initial commit

--- shell.c
+++ shell.c
@@ -4,19 +4,21 @@
 
 int main(void)
 {
-	char *buf = malloc(512); /* Empty buffer for Getline to use */
+	char *buf; /* Empty buffer for Getline to use */
 	size_t len = 0; /* Getline will handle realloc */
-	size_t bytes; /* Will stores bytes read by getline */
-	char **program = malloc(512);
-	int index = 0;
+	ssize_t bytes; /* Will stores bytes read by getline */
+	char *program[64];
+	int index;
 	int status; /* Will store the status of execve */
+	char *delimeter = " ";
+	char *splitted;
 
 	/* Get the inital dollar sign */
 	printf("$ ");
 
 	/* Getline reads (aka copies) everything from stdin into buf */
 	/* @bytes: stores the number of chars reads */
-	while(bytes = getline(&buf, &len, stdin) > 0)
+	while((bytes = getline(&buf, &len, stdin)) > 0)
 	{
 		/* Check if getline failed */
 		if (bytes == -1)
@@ -25,20 +27,28 @@ int main(void)
 			free(buf);
 			exit(99);
 		}
-		/* split the string into argvs */
-		while (index < 3)
+		/* splitting string */
+		splitted = strtok(buf, delimeter);
+		index = 0;
+		while (splitted != NULL)
 		{
-			program[index] = *splitter(buf, " ");
+			program[index] = splitted;
+			printf("program[%d] = %s\n", index, program[index]);
+			printf("splitted = %s\n", splitted);
 			index++;
+
+        		splitted = strtok(NULL, delimeter);
 		}
 		/* execute it */
-		execve(program[0], program, NULL);
-		if (status = -1)
+		status = execve(program[0], program, NULL);
+		if (status == -1)
 		{
-			dprintf(STDERR_FILENO, "%s: not found/n", program[0]);
+			dprintf(STDERR_FILENO, "%s: not found\n", program[0]);
 		}
 		/* Get the dollar sign for the the next prompt */
 		printf("$ ");
 	}
+	free(*program);
+	free(buf);
 	return (0);
 }
