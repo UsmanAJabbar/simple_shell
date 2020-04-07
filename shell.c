f7d71d8e662e128e619479d37a00a3c92e79e7bb
UsmanGTA <usmangta@gmail.com>
2020-04-07 13:14:05 +0000

    latest code push

--- shell.c
+++ shell.c
@@ -1,55 +1,43 @@
 #include "implicit_declarations_HQ.h"
-#define FORK_F write(1,"Failed to fork PID\n", 20)
+
+#define FORK_F write(1, "Failed to fork PID\n", 20)
 #define WAITPID waitpid(child, &pidstatus, WUNTRACED)
 #define EXEC (status = execve(args[0], args, NULL))
 #define EXEC_F dprintf(STDERR_FILENO, "%s: not found\n", args[0])
 #define PROMPT "$ "
-
-void func(int name)
-{
-	(void)name;
-	printf("\n$ ");
-}
 
 /**
  * main - a simple shell program that
  * runs user inputs, parses, and executes them
  * Return: - Always 0
  */
-int main(void)
+int main(int argc, char *argv[], char *envp[])
 {
 	char *args[64]; /* Array that would store argv inputs */
-	char *input = NULL, *splitted; /* Buf for getline | Temp for each strtok argv */
-	char bin[] = "/bin/";
+	char *in = NULL, *splitted; /* Buf for getline |Temp for each strtok argv*/
 	size_t len = 0; /* Getline will handle realloc */
 	int bytes, status, index, pidstatus; /*Stores ? strlen | exec status | index*/
 	pid_t child; /* Generates and saves the child PID status */
 
-	write(1, "initial $", 11); /* Get the inital dollar sign */
-	signal(SIGINT, func);
-	/* Getline reads (aka copies) everything from stdin into input */
-	while ((bytes = getline(&input, &len, stdin)) > 0)
+	signal(SIGINT, ctrlc); /* Handles Ctrl-C Interrupt */
+	while (1)
 	{
-		char bin[] = "/bin/";
-		input[bytes - 1] = '\0'; /* Replaces /n with '\0' */
+		write(1, "initial $", 11); /* Get the inital dollar sign */
 
-		printf("Before strcmp/cat getline copied %d bytes into input\n", bytes);
-
-		/* Cat if input has doesn't have /bin/ present ahead of it */
-		if(_strncmp(bin, input, _strlen(bin)) != 0)
-			_strcat(bin, input);
-
-		printf("Bin has: %s\n", bin);
+		/* Getline reads (aka copies) everything from stdin into input */
+		bytes = getline(&in, &len, stdin);
+		in[bytes - 1] = '\0';
 
 		/* Generate *argv[]s */
-		splitted = strtok(bin, " ");
+		splitted = strtok(in, " ");
 		for (index = 0; splitted != NULL; index++)
 			args[index] = splitted, splitted = strtok(NULL, " ");
 		args[index] = '\0';
+
 		/* Create a child process, execute it, reset status on fail */
 		((child = fork()) < 0) ? FORK_F : (child == 0) ? EXEC : WAITPID;
-		(status < 0) ? EXEC_F, write(1, PROMPT, 3), status = 0, _memset(bin, 0, sizeof(bin)) : write(1, PROMPT, 3), _memset(bin, 0, sizeof(bin));
-
+		if (status < 0)
+			EXEC_F, status = 0;
 	}
 	return (0);
 }
