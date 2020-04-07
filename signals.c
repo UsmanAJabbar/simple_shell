c7134cf237d85fa9e7b4f44f1c80776db26e5efb
UsmanGTA <usmangta@gmail.com>
2020-04-07 13:16:31 +0000

    add

---
+++ signals.c
@@ -0,0 +1,12 @@
+/**
+ * ctrlc - void function that takes the singal interrupt (aka Ctrl-C)
+ * and loads it onto the wrong plane
+ * @name: the plane you throught boarded on
+ * Return: Always Void
+ * just like that luggage you're never getting back
+ */
+void ctrlc(int name)
+{
+	(void)name;
+	write(1, "\n$ ", 1);
+}
