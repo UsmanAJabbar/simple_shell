5955c9b4e90a3ad2b65b24d092e24ef18f185379
UsmanGTA <usmangta@gmail.com>
2020-04-08 10:31:54 +0000

    need to fix exit code

--- shell.c
+++ shell.c
@@ -2,21 +2,22 @@
 
 #define FORK_F write(1, "Failed to fork PID\n", 20)
 #define WAITPID waitpid(child, &pidstatus, WUNTRACED)
-#define EXEC (status = execve(args[0], args, NULL))
+#define EXEC (execstatus = execve(args[0], args, NULL))
 #define EXEC_F dprintf(STDERR_FILENO, "%s: not found\n", args[0])
 #define PROMPT "$ "
+#define CHILDSTATUS (child = fork())
 
 /**
  * main - a simple shell program that
  * runs user inputs, parses, and executes them
  * Return: - Always 0
  */
-int main(int argc, char *argv[], char *envp[])
+int main(int ac, char **av, char **env)
 {
-	char *args[64]; /* Array that would store argv inputs */
+	char *args[64], *environment[100]; /* Array that would store argv inputs */
 	char *in = NULL, *splitted; /* Buf for getline |Temp for each strtok argv*/
 	size_t len = 0; /* Getline will handle realloc */
-	int bytes, status, index, pidstatus; /*Stores ? strlen | exec status | index*/
+	int bytes, execstatus, index, pidstatus; /*Stores ? strlen|execstatus|index*/
 	pid_t child; /* Generates and saves the child PID status */
 
 	signal(SIGINT, ctrlc); /* Handles Ctrl-C Interrupt */
@@ -25,19 +26,25 @@ int main(int argc, char *argv[], char *envp[])
 		write(1, "initial $", 11); /* Get the inital dollar sign */
 
 		/* Getline reads (aka copies) everything from stdin into input */
-		bytes = getline(&in, &len, stdin);
-		in[bytes - 1] = '\0';
+		if ((bytes = getline(&in, &len, stdin)) >= 0)
+			in[bytes - 1] = '\0';
+		else
+			break;
 
 		/* Generate *argv[]s */
 		splitted = strtok(in, " ");
 		for (index = 0; splitted != NULL; index++)
 			args[index] = splitted, splitted = strtok(NULL, " ");
-		args[index] = '\0';
+		args[index] = NULL;
+
+		if (_strncmp(args[0], "exit", 4)
+			exit(0);
 
 		/* Create a child process, execute it, reset status on fail */
-		((child = fork()) < 0) ? FORK_F : (child == 0) ? EXEC : WAITPID;
-		if (status < 0)
-			EXEC_F, status = 0;
+		(CHILDSTATUS < 0) ? FORK_F : (child > 0) ? WAITPID : EXEC;
+		if (execstatus < 0)
+			EXEC_F, execstatus = 0;
 	}
+	write(1, "\n", 1), free(in), exit(pidstatus);
 	return (0);
 }
